0c09d449ccb55779a429a55a4fb9cd1d27cbbebe
Dexter Lowe <feldoh10@gmail.com>
2024-07-11 22:15:15 +0100

    No pawn rewards
    there's 3 main ways pawns can be added by the looks of it:
    QuestNode_PawnsArrive
    QuestNode_AddPawnReward
    QuestNode_GiveRewards
    
    The first two we may just be able to patch out.
    The last one is used for most of the quests and has params:
    thingRewardDisallowed
    thingRewardRequired
    thingRewardItemsOnly
    allowRoyalFavor
    allowGoodwill
    allowDevelopmentPoints
    
    Now there's a few things here:
    Imo we should set globally:
    allowRoyalFavor: False
    allowGoodwill: False
    
    Anything with thingRewardDisallowed should probably be set to allowDevelopmentPoints: True to make sure there's at least one prize
    In general though if you see a quest generating a pawn setting thingRewardItemsOnly in the parms should remove pawns as options.
    
    I think just applying thingRewardItemsOnly to all quest parms might actually be enough to fix this with nothing extra needed
    
    This commit just does that globally right at the top as a prefix which _might_ be enough

---
+++ 1.5/Source/Ragnarok/HarmonyPatches/NoPawnRewardsPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using RimWorld;
+using RimWorld.QuestGen;
+
+namespace Ragnarok.HarmonyPatches;
+
+public static class NoPawnRewardsPatch
+{
+    [HarmonyPatch(typeof(QuestGen_Rewards), "GenerateRewards")]
+    public static class QuestGen_Rewards_Patch
+    {
+        [HarmonyPrefix]
+        public static bool PrefixGenerateRewards(ref RewardsGeneratorParams parmsResolved)
+        {
+            parmsResolved.thingRewardItemsOnly = true;
+            return true;
+        }
+    }
+}
