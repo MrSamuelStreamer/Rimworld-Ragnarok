15b0347e1982de91201beab9ac04c9fc9339267e
Dexter Lowe <feldoh10@gmail.com>
2024-07-16 01:35:41 +0100

    Darkness seeps in event

---
+++ 1.5/Source/Ragnarok/IncidentWorker_DarknessSeepsIn.cs
@@ -0,0 +1,33 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Ragnarok;
+
+public class IncidentWorker_DarknessSeepsIn : IncidentWorker
+{
+    protected override bool CanFireNowSub(IncidentParms parms) =>
+        FindRandomLightSource((Map)parms.target) != null;
+
+    protected override bool TryExecuteWorker(IncidentParms parms)
+    {
+        Map map = (Map)parms.target;
+        Thing lightSource = FindRandomLightSource(map);
+        if (lightSource == null)
+            return false;
+
+        CompRefuelable fuelComp = lightSource.TryGetComp<CompRefuelable>();
+        fuelComp.ConsumeFuel(fuelComp.Fuel);
+
+        SendStandardLetter(
+            "MSSRAG_LetterLabel_Incident_DarknessSeepsIn".Translate(),
+            "MSSRAG_Letter_Incident_DarknessSeepsIn".Translate(lightSource.def.label),
+            LetterDefOf.NegativeEvent, parms, new TargetInfo(lightSource.Position, map));
+        return true;
+    }
+
+    public static Thing FindRandomLightSource(Map map) =>
+        map.listerBuildings
+            .allBuildingsColonist
+            .Find(b => b.TryGetComp<CompGlower>() is { Glows: true } &&
+                       b.TryGetComp<CompRefuelable>() is { HasFuel: true });
+}
