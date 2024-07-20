21af835ad62fe6d3d90c76a717a213dd6e35387c
Kieran David Evans <keyz182@gmail.com>
2024-07-20 15:52:26 +0100

    Bioferrite tattoos

---
+++ 1.5/Source/Ragnarok/Tattoo/HediffCompProperties_Tattoo.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Ragnarok.Tattoo;
+
+public class HediffCompProperties_Tattoo : HediffCompProperties
+{
+    public bool OverrideExistingTattoo = false;
+    public TattooType TattooType = TattooType.Body;
+    public TattooDef Tattoo = null;
+    
+    public HediffCompProperties_Tattoo()
+    {
+        this.compClass = typeof (HediffComp_Tattoo);
+    }
+    
+}
---
+++ 1.5/Source/Ragnarok/Tattoo/HediffComp_Tattoo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using Verse;
+
+namespace Ragnarok.Tattoo;
+
+public class HediffComp_Tattoo: HediffComp
+{
+    private HediffCompProperties_Tattoo Props => (HediffCompProperties_Tattoo) props;
+    
+    
+    public override void CompPostMake()
+    {
+        // Don't overwrite existing tattoos, unless OverrideExistingTattoo
+        if(Props.TattooType == TattooType.Body && Pawn.style.BodyTattoo != null && !Props.OverrideExistingTattoo) return;
+        if(Props.TattooType == TattooType.Face && Pawn.style.FaceTattoo != null && !Props.OverrideExistingTattoo) return;
+
+        TattooDef newTattoo;
+        if (Props.Tattoo != null)
+        {
+            newTattoo = Props.Tattoo;
+        }
+        else
+        {
+            // Simplified version of the random tattoo getter
+            IEnumerable<TattooDef> relevantTattoos =
+                DefDatabase<TattooDef>.AllDefs.Where(x => PawnStyleItemChooser.WantsToUseStyle(Pawn, x, Props.TattooType));
+
+            newTattoo = relevantTattoos.RandomElementWithFallback();
+        }
+
+        if (Props.TattooType == TattooType.Body)
+        {
+            // bypass setter to override ideology
+            Pawn.style.bodyTattoo = newTattoo;
+        }
+        else
+        {
+            // bypass setter to override ideology
+            Pawn.style.faceTattoo = newTattoo;
+        }
+    }
+}
