76753bc79435b664650ad4f527226367637ff6de
Kieran David Evans <keyz182@gmail.com>
2024-07-23 14:45:07 +0100

    Make the settings fix button use the debug button code.

--- 1.5/Source/Ragnarok/Settings.cs
+++ 1.5/Source/Ragnarok/Settings.cs
@@ -17,33 +17,13 @@ public class Settings : ModSettings
         options.Begin(wrect);
         if (Current.ProgramState == ProgramState.Playing &&
             ModLister.HasActiveModWithName("Medieval Overhaul") &&
-            options.ButtonText("MSSRAG_Settings_RecreateAnimaTrees".Translate(), widthPct: 0.3f)) FixAnimaTrees();
+            Find.CurrentMap != null &&
+            options.ButtonText("MSSRAG_Settings_RecreateAnimaTrees".Translate(), widthPct: 0.3f)) DebugTools.AnimaTreeFix();
         options.CheckboxLabeled("MSSRAG_Settings_RemoveSun".Translate(), ref removeSun);
         options.CheckboxLabeled("MSSRAG_Settings_RemovePawnRewards".Translate(), ref removePawnRewards);
         options.Gap();
 
         options.End();
-    }
-
-    private void FixAnimaTrees()
-    {
-        Map map = Find.CurrentMap;
-        map.listerThings.ThingsOfDef(ThingDefOf.ArchonexusCore).ForEach(t =>
-        {
-            if (t.TryGetComp<CompSpawnImmortalSubplantsAround>() is not { } comp || comp.Props.subplant != ThingDefOf.Plant_TreeAnima) return;
-            List<IntVec3> cells = AccessTools.Field(typeof(CompSpawnImmortalSubplantsAround), "cells").GetValue(comp) as List<IntVec3>;
-            if (cells == null) return;
-            foreach (IntVec3 cell in cells)
-            {
-                if (cell.GetThingList(map).Find(thing => thing.def == ThingDefOf.Plant_TreeAnima) is not Plant plant) continue;
-                Plant newTree = ThingMaker.MakeThing(ThingDefOf.Plant_TreeAnima) as Plant;
-                if (newTree == null) continue;
-                newTree.Growth = plant.Growth;
-                newTree.Age = plant.Age;
-                plant.Destroy();
-                GenSpawn.Spawn(newTree, cell, map, WipeMode.Vanish);
-            }
-        });
     }
 
     public override void ExposeData()
